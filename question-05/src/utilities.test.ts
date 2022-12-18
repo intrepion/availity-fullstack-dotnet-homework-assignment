c1f22abd224be797163af5b6d28df220bf804c15
Oliver Forral <intrepion@gmail.com>
2022-12-18 16:01:25 -0700

    red

--- question-05/src/utilities.test.ts
+++ question-05/src/utilities.test.ts
@@ -5,3 +5,9 @@ test('NPI number not long enough', () => {
     let valid = utilities.validateNPI(npiNumber);
     expect(valid).toBe(false);
 });
+
+test('NPI number too long', () => {
+    let npiNumber = '12345678901';
+    let valid = utilities.validateNPI(npiNumber);
+    expect(valid).toBe(false);
+});
