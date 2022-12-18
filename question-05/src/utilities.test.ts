9f395fd3930541db57ae062a6e29724a61655df8
Oliver Forral <intrepion@gmail.com>
2022-12-18 15:59:53 -0700

    red

--- question-05/src/utilities.test.ts
+++ question-05/src/utilities.test.ts
@@ -3,4 +3,5 @@ import utilities from './utilities';
 test('NPI number not long enough', () => {
     let npiNumber = '123456789';
     let valid = utilities.validateNPI(npiNumber);
+    expect(valid).toBe(false);
 });
