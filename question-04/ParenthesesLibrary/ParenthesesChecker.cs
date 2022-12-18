0c47d4b5984f73a2a76a938f06abb9794055a077
Oliver Forral <intrepion@gmail.com>
2022-12-18 15:23:57 -0700

    green

--- question-04/ParenthesesLibrary/ParenthesesChecker.cs
+++ question-04/ParenthesesLibrary/ParenthesesChecker.cs
@@ -5,11 +5,18 @@ public class ParenthesesChecker
     public static bool Check(string code)
     {
         var level = 0;
+        var started = false;
         for (int i = 0; i < code.Length; i++)
         {
             if (code[i] == '(')
             {
+                if (level == 0 && started)
+                {
+                    return false;
+                }
+
                 level += 1;
+                started = true;
             }
             else if (code[i] == ')')
             {
