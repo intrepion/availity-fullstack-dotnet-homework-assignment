504738b105fca87d5266ac9a26fd016eeabedc38
Oliver Forral <intrepion@gmail.com>
2022-12-18 15:13:59 -0700

    green

--- question-04/ParenthesesLibrary/ParenthesesChecker.cs
+++ question-04/ParenthesesLibrary/ParenthesesChecker.cs
@@ -4,6 +4,19 @@ public class ParenthesesChecker
 {
     public static bool Check(string code)
     {
-        return true;
+        var level = 0;
+        for (int i = 0; i < code.Length; i++)
+        {
+            if (code[i] == '(')
+            {
+                level += 1;
+            }
+            else if (code[i] == ')')
+            {
+                level -= 1;
+            }
+        }
+
+        return level == 0;
     }
 }
