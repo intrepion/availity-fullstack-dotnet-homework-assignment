aac327589f165c8a315f2352d012970f270318b6
Oliver Forral <intrepion@gmail.com>
2022-12-18 18:49:21 -0700

    npx prettier --write .

--- question-05/src/utilities.test.ts
+++ question-05/src/utilities.test.ts
@@ -1,19 +1,19 @@
-import utilities from './utilities';
+import utilities from "./utilities";
 
-test('NPI number not long enough', () => {
-    let npiNumber = '123456789';
-    let valid = utilities.validateNPI(npiNumber);
-    expect(valid).toBe(false);
+test("NPI number not long enough", () => {
+  let npiNumber = "123456789";
+  let valid = utilities.validateNPI(npiNumber);
+  expect(valid).toBe(false);
 });
 
-test('NPI number too long', () => {
-    let npiNumber = '12345678901';
-    let valid = utilities.validateNPI(npiNumber);
-    expect(valid).toBe(false);
+test("NPI number too long", () => {
+  let npiNumber = "12345678901";
+  let valid = utilities.validateNPI(npiNumber);
+  expect(valid).toBe(false);
 });
 
-test('NPI number has non-digits', () => {
-    let npiNumber = 'lZ34SG7Bg0';
-    let valid = utilities.validateNPI(npiNumber);
-    expect(valid).toBe(false);
+test("NPI number has non-digits", () => {
+  let npiNumber = "lZ34SG7Bg0";
+  let valid = utilities.validateNPI(npiNumber);
+  expect(valid).toBe(false);
 });
--- question-05/src/utilities.ts
+++ question-05/src/utilities.ts
@@ -1,26 +1,26 @@
 var utilities = {
-    validateNPI: (npiNumber: string) => {
-        if (npiNumber.length < 10) {
-            return false;
-        }
-
-        if (npiNumber.length > 10) {
-            return false;
-        }
-
-        const zeroCharCode = "0".charCodeAt(0);
-        const nineCharCode = "9".charCodeAt(0);
-        const length = npiNumber.length;
-
-        for (let i = 0; i < length; i += 1) {
-            const d = npiNumber.charCodeAt(i);
-            if (d < zeroCharCode || d > nineCharCode) {
-                return false
-            }
-        }
-
-        return true;
+  validateNPI: (npiNumber: string) => {
+    if (npiNumber.length < 10) {
+      return false;
     }
-}
+
+    if (npiNumber.length > 10) {
+      return false;
+    }
+
+    const zeroCharCode = "0".charCodeAt(0);
+    const nineCharCode = "9".charCodeAt(0);
+    const length = npiNumber.length;
+
+    for (let i = 0; i < length; i += 1) {
+      const d = npiNumber.charCodeAt(i);
+      if (d < zeroCharCode || d > nineCharCode) {
+        return false;
+      }
+    }
+
+    return true;
+  },
+};
 
 export default utilities;
