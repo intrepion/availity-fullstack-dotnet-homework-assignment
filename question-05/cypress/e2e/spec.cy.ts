f726ad1a237f39d98a7d46ddcb34d130c122e17e
Oliver Forral <intrepion@gmail.com>
2022-12-18 20:43:50 -0700

    red

--- question-05/cypress/e2e/spec.cy.ts
+++ question-05/cypress/e2e/spec.cy.ts
@@ -9,6 +9,9 @@ describe("registration", () => {
     cy.get("input#firstNameBox").type("John");
     cy.get("input#lastNameBox").type("Doe");
     cy.get("input#npiNumberBox").type("1234567890");
+    cy.get("input#businessAddressBox").type("123 Main St{enter}Beverly Hills, CA 90210");
+    cy.get("input#telephoneNumberBox").type("123-456-7890");
+    cy.get("input#emailAddressBox").type("some@email.com");
     cy.get("button#register").click();
     cy.get("div#registrationStatus").should("exist");
     cy.get("div#registrationStatus p#firstNameStatus").should(
@@ -22,6 +25,18 @@ describe("registration", () => {
     cy.get("div#registrationStatus p#npiNumberStatus").should(
       "contain",
       "NPI Number: 1234567890"
+    );
+    cy.get("div#registrationStatus p#businessAddressStatus").should(
+      "contain",
+      "Business Address: 123 Main St\nBeverly Hills, CA 90210"
+    );
+    cy.get("div#registrationStatus p#telephoneNumberStatus").should(
+      "contain",
+      "Telephone Number: 123-456-7890"
+    );
+    cy.get("div#registrationStatus p#emailAddressStatus").should(
+      "contain",
+      "Email Address: some@email.com"
     );
   });
 
@@ -45,12 +60,42 @@ describe("registration", () => {
     cy.get("div#registrationStatus").should("not.exist");
   });
 
-  it("errors when there is a missing NPI number", () => {
+  it("errors when there is an invalid NPI number", () => {
     cy.visit("http://localhost:3000/");
     cy.get("button#register").click();
     cy.get("span#npiNumberError").should(
       "contain",
       "Please enter valid NPI number."
+    );
+    cy.get("div#registrationStatus").should("not.exist");
+  });
+
+  it("errors when there is a missing business address", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("button#register").click();
+    cy.get("span#businessAddressError").should(
+      "contain",
+      "Please enter business address."
+    );
+    cy.get("div#registrationStatus").should("not.exist");
+  });
+
+  it("errors when there is a missing telephone number", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("button#register").click();
+    cy.get("span#telephoneNumberError").should(
+      "contain",
+      "Please enter telephone number."
+    );
+    cy.get("div#registrationStatus").should("not.exist");
+  });
+
+  it("errors when there is a missing email address", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("button#register").click();
+    cy.get("span#emailAddressError").should(
+      "contain",
+      "Please enter email address."
     );
     cy.get("div#registrationStatus").should("not.exist");
   });
