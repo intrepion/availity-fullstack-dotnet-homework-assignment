d09ed4fa7b4499f6a929720221514ae3be91eb95
Oliver Forral <intrepion@gmail.com>
2022-12-18 18:59:56 -0700

    green

---
+++ question-05/src/RegistrationForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RegistrationForm() {
+
+    return (
+        <div>
+            <h1>Registration</h1>
+        </div>
+    );
+}
+
+export default RegistrationForm;
