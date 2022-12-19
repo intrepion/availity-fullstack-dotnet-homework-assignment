d0e357881d5356bc6a76b8dfefe26053f4df2313
Oliver Forral <intrepion@gmail.com>
2022-12-18 19:06:10 -0700

    red

--- question-05/cypress/e2e/spec.cy.ts
+++ question-05/cypress/e2e/spec.cy.ts
@@ -7,5 +7,6 @@ describe("registration", () => {
   it("errors when there is a missing first name", () => {
     cy.visit("http://localhost:3000/");
     cy.get("button#register").click();
+    cy.get("span#firstNameError").should("contain", "Please enter the first name.");
   });
 });
