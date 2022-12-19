da3b9dbf0a669e77c6b16aec73afa24e1da48088
Oliver Forral <intrepion@gmail.com>
2022-12-18 21:58:57 -0700

    red

--- question-06/EnrollmentTests/ConversionTests.cs
+++ question-06/EnrollmentTests/ConversionTests.cs
@@ -5,7 +5,7 @@ namespace EnrollmentTests;
 public class ConversionTests
 {
     [Theory]
-    [InlineData("Aaron Andrews", "Alice Bob", "Aaron Andrews", "Alice Bob")]
+    [InlineData("Alice Andrews", "Aaron Bob", "Alice Andrews", "Aaron Bob")]
     public void FirstAndLastNameSortTest(string input1, string input2, string expected1, string expected2)
     {
         var enrollees = new List<Enrollee>
@@ -15,5 +15,8 @@ public class ConversionTests
         };
 
         var sorted = EnrolleeSorter.Sort(enrollees);
+
+        Assert.Equal(expected1, sorted[0].Name);
+        Assert.Equal(expected2, sorted[1].Name);
     }
 }
