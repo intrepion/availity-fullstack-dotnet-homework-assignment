cc8044d67a3ffa45d8aeecd97b06692bcc265510
Oliver Forral <intrepion@gmail.com>
2022-12-18 20:13:20 -0700

    green

--- question-05/cypress/e2e/spec.cy.ts
+++ question-05/cypress/e2e/spec.cy.ts
@@ -7,22 +7,32 @@ describe("registration", () => {
   it("successfully shows registration details", () => {
     cy.visit("http://localhost:3000/");
     cy.get("input#firstNameBox").type("John");
+    cy.get("input#lastNameBox").type("Doe");
     cy.get("button#register").click();
-    cy.get("div#registrationStatus h2").should("contain", "Registration Details");
-    cy.get("div#registrationStatus p#firstNameStatus").should("contain", "First Name: John");
+    cy.get("div#registrationStatus").should("exist");
+    cy.get("div#registrationStatus p#firstNameStatus").should(
+      "contain",
+      "First Name: John"
+    );
   });
 
   it("errors when there is a missing first name", () => {
     cy.visit("http://localhost:3000/");
     cy.get("button#register").click();
-    cy.get("span#firstNameError").should("contain", "Please enter the first name.");
+    cy.get("span#firstNameError").should(
+      "contain",
+      "Please enter the first name."
+    );
     cy.get("div#registrationStatus").should("not.exist");
   });
 
   it("errors when there is a missing last name", () => {
     cy.visit("http://localhost:3000/");
     cy.get("button#register").click();
-    cy.get("span#lastNameError").should("contain", "Please enter the last name.");
+    cy.get("span#lastNameError").should(
+      "contain",
+      "Please enter the last name."
+    );
     cy.get("div#registrationStatus").should("not.exist");
   });
 });
--- question-05/src/RegistrationForm.tsx
+++ question-05/src/RegistrationForm.tsx
@@ -1,62 +1,115 @@
 import React, { useState } from "react";
 
 function RegistrationForm() {
-    const [firstName, setFirstName] = useState("");
-    const [firstNameError, setFirstNameError] = useState("");
-    const [firstNameSubmitted, setFirstNameSubmitted] = useState("");
-    const [registrationSuccess, setRegistrationSuccess] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [firstNameSubmitted, setFirstNameSubmitted] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [lastNameSubmitted, setLastNameSubmitted] = useState("");
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
 
-    const isEmpty = (value: string) => {
-        return value === "";
+  const isEmpty = (value: string) => {
+    return value === "";
+  };
+
+  const validateFirstName = () => {
+    if (isEmpty(firstName)) {
+      setFirstNameError("Please enter the first name.");
+    } else {
+      setFirstNameError("");
+    }
+  };
+
+  const validateLastName = () => {
+    if (isEmpty(lastName)) {
+      setLastNameError("Please enter the first name.");
+    } else {
+      setLastNameError("");
+    }
+  };
+
+  const handleFirstNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    let value = event.target.value;
+    setFirstName(value);
+    validateFirstName();
+  };
+
+  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let value = event.target.value;
+    setLastName(value);
+    validateLastName();
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    validateFirstName();
+    validateLastName();
+
+    if (isEmpty(firstName)) {
+      setRegistrationSuccess(false);
+      return;
     }
 
-    const validateFirstName = () => {
-        if (isEmpty(firstName)) {
-            setFirstNameError("Please enter the first name.");
-        } else {
-            setFirstNameError("");
-        }
+    if (isEmpty(lastName)) {
+      setRegistrationSuccess(false);
+      return;
     }
 
-    const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        let value = event.target.value;
-        setFirstName(value);
-        validateFirstName();
-    }
+    setFirstNameSubmitted(firstName);
+    setLastNameSubmitted(lastName);
+    setRegistrationSuccess(true);
+  };
 
-    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        validateFirstName();
-        if (isEmpty(firstName)) {
-            setRegistrationSuccess(false);
-        } else {
-            setFirstNameSubmitted(firstName);
-            setRegistrationSuccess(true);
-        }
-    }
-
-    return (
-        <div>
-            <h1>Registration</h1>
-            <form onSubmit={handleFormSubmit}>
-                <p>
-                    <label htmlFor="firstNameBox">First Name: </label>
-                    <input type="text" id="firstNameBox" name="firstNameBox" onChange={handleFirstNameChange} value={firstName} />
-                </p>
-                <p>
-                    {firstNameError !== "" && <span id="firstNameError">Please enter the first name.</span>}
-                </p>
-                <p>
-                    <button id="register">Register</button>
-                </p>
-            </form>
-            {registrationSuccess && 
-                <div id="registrationStatus">
-                    <h2>Registration Details</h2>
-                    <p id="firstNameStatus">First Name: {firstNameSubmitted}</p>
-                </div>}
+  return (
+    <div>
+      <h1>Registration</h1>
+      <form onSubmit={handleFormSubmit}>
+        <p>
+          <label htmlFor="firstNameBox">First Name: </label>
+          <input
+            type="text"
+            id="firstNameBox"
+            name="firstNameBox"
+            onChange={handleFirstNameChange}
+            value={firstName}
+          />
+          {firstNameError !== "" && (
+            <span className="error" id="firstNameError">
+              Please enter the first name.
+            </span>
+          )}
+        </p>
+        <p>
+          <label htmlFor="lastNameBox">Last Name: </label>
+          <input
+            type="text"
+            id="lastNameBox"
+            name="lastNameBox"
+            onChange={handleLastNameChange}
+            value={lastName}
+          />
+          {lastNameError !== "" && (
+            <span className="error" id="lastNameError">
+              Please enter the last name.
+            </span>
+          )}
+        </p>
+        <p>
+          <button id="register">Register</button>
+        </p>
+      </form>
+      {registrationSuccess && (
+        <div id="registrationStatus">
+          <h2>Registration Details</h2>
+          <p id="firstNameStatus">First Name: {firstNameSubmitted}</p>
+          <p id="lastNameStatus">Last Name: {lastNameSubmitted}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default RegistrationForm;
