7849fdf003f06906a23156d4ba5282201c5b0dd5
Oliver Forral <intrepion@gmail.com>
2022-12-18 18:55:29 -0700

    red

--- question-05/cypress/e2e/spec.cy.ts
+++ question-05/cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("registration", () => {
+  it("loads", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("h1").should("contain", "Registration");
+  });
+});
