333edc544837660d41b3e7aa916b0fe07c99bb27
Oliver Forral <intrepion@gmail.com>
2022-12-18 23:28:46 -0700

    red

--- question-06/EnrollmentTests/EnrolleeSeparatorTests.cs
+++ question-06/EnrollmentTests/EnrolleeSeparatorTests.cs
@@ -5,13 +5,14 @@ namespace EnrollmentTests;
 public class EnrolleeSeparatorTests
 {
     [Theory]
-    [InlineData("Some Company", "1", "Another Company", "2", 2)]
-    public void EnrolleeSeparatorTest(string company1, string id1, string company2, string id2, int expected)
+    [InlineData("Some Company", "1", 1, "Another Company", "1", 1, 2)]
+    [InlineData("Some Company", "1", 1, "Some Company", "1", 1, 1)]
+    public void EnrolleeSeparatorTest(string company1, string id1, int version1, string company2, string id2, int version2, int expected)
     {
         var enrollees = new List<Enrollee>
         {
-            new Enrollee { Company = company1, Id = id1, Name = "Alice Bob" },
-            new Enrollee { Company = company2, Id = id2, Name = "Bob Alice" }
+            new Enrollee { Company = company1, Id = id1, Version = version1, Name = "Alice Bob" },
+            new Enrollee { Company = company2, Id = id2, Version = version2, Name = "Bob Alice" }
         };
 
         var reduced = EnrolleeSeparator.Separate(enrollees);
