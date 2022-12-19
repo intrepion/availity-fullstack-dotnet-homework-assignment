95ef4fe55b2fc868a6aadee71877d82edf1ec6f1
Oliver Forral <intrepion@gmail.com>
2022-12-18 19:48:23 -0700

    red

--- question-05/cypress/e2e/spec.cy.ts
+++ question-05/cypress/e2e/spec.cy.ts
@@ -8,13 +8,14 @@ describe("registration", () => {
     cy.visit("http://localhost:3000/");
     cy.get("input#firstNameBox").type("John");
     cy.get("button#register").click();
-    cy.get("h2#registrationStatus").should("contain", "Registration Details");
+    cy.get("div#registrationStatus h2").should("contain", "Registration Details");
+    cy.get("div#registrationStatus p.firstNameStatus").should("contain", "First Name: John");
   });
 
   it("errors when there is a missing first name", () => {
     cy.visit("http://localhost:3000/");
     cy.get("button#register").click();
     cy.get("span#firstNameError").should("contain", "Please enter the first name.");
-    cy.get("h2#registrationStatus").should("not.exist");
+    cy.get("div#registrationStatus").should("not.exist");
   });
 });
